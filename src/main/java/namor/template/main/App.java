63af067575702f1ca72aef490c0de68ad5a31a6e
NamorV <drednout309@gmail.com>
2017-03-22 18:37:44 +0100

    refactoring

--- src/main/java/namor/template/main/App.java
+++ src/main/java/namor/template/main/App.java
@@ -11,7 +11,7 @@ public class App {
         return minuend - subtrahend;
     }
 
-    public int multiplicate(int a, int b) {
-        return a*b;
+    public int multiplicate(int multiplicand, int multiplier) {
+        return multiplicand * multiplier;
     }
 }
