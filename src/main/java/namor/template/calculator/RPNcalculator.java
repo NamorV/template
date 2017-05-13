c89d3c8bb03751431382deca82eb6531f1144ec5
NamorV <drednout309@gmail.com>
2017-05-13 16:41:02 +0200

    added isCorrect() method

--- src/main/java/namor/template/calculator/RPNcalculator.java
+++ src/main/java/namor/template/calculator/RPNcalculator.java
@@ -1,4 +1,16 @@
 package namor.template.calculator;
 
 public class RPNcalculator {
+
+    public boolean isCorrect(String rpnString){
+        for(int i = 0; i < rpnString.length(); i++){
+            char x = rpnString.charAt(i);
+            if(!(Character.isDigit(x) || x == '/' || x == '*' ||
+                    x == '+'|| x== '-' || x=='%' || x == '\n' ||
+                    x == ' ')){
+                return false;
+            }
+        }
+        return true;
+    }
 }
