9a36bfb7fbb794adfcc0ee9399b71b223ff28c4a
NamorV <drednout309@gmail.com>
2017-06-02 17:34:56 +0200

    code review fixes

--- src/main/java/namor/template/calculator/RPNcalculator.java
+++ src/main/java/namor/template/calculator/RPNcalculator.java
@@ -3,58 +3,60 @@ package namor.template.calculator;
 import namor.template.stack.ArrayStack;
 
 public class RPNcalculator {
-    private ArrayStack numbers = new ArrayStack();
+
 
     public int calculate(String arithmeticExpression){
-        for(String splitData : arithmeticExpression.split(" ")){
-            if(Sign.isSign(splitData)){
-                doArithmeticOperation(splitData);
+        ArrayStack numbers = new ArrayStack();
+
+        for(String operationElement : arithmeticExpression.split(" ")){
+            if(Sign.isSign(operationElement)){
+                doArithmeticOperation(operationElement, numbers);
             } else {
-                numbers.push(Integer.parseInt(splitData));
+                numbers.push(Integer.parseInt(operationElement));
             }
         }
         return numbers.pop();
     }
 
-    private void doArithmeticOperation(String arithmeticOperator) {
+    private void doArithmeticOperation(String arithmeticOperator, ArrayStack numbers) {
         switch (Sign.getValue(arithmeticOperator)){
             case ADDITION:
-                numbers.push(addition());
+                numbers.push(addition(numbers));
                 break;
             case SUBTRACTION:
-                numbers.push(subtraction());
+                numbers.push(subtraction(numbers));
                 break;
             case MULTIPLICATION:
-                numbers.push(multiplication());
+                numbers.push(multiplication(numbers));
                 break;
             case DIVISION:
-                numbers.push(division());
+                numbers.push(division(numbers));
                 break;
         }
     }
 
-    private final int addition(){
+    private final int addition(ArrayStack numbers){
         final int secondNumber = numbers.pop();
         final int firstNumber = numbers.pop();
 
         return firstNumber + secondNumber;
     }
 
-    private final int subtraction(){
+    private final int subtraction(ArrayStack numbers){
         final int secondNumber = numbers.pop();
         final int firstNumber = numbers.pop();
         
         return firstNumber - secondNumber;
     }
 
-    private final int multiplication(){
+    private final int multiplication(ArrayStack numbers){
         final int  secondNumber = numbers.pop();
         final int firstNumber = numbers.pop();
         
         return firstNumber * secondNumber;
     }
 
-    private final int division(){
+    private final int division(ArrayStack numbers){
         final int secondNumber = numbers.pop();
         final int firstNumber = numbers.pop();
 
