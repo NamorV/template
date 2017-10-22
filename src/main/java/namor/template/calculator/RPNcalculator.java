4f97d72405d4208f8ea9c6f27ae6f7822f52d6a4
NamorV <drednout309@gmail.com>
2017-10-22 16:06:26 +0200

    refactoring

--- src/main/java/namor/template/calculator/RPNcalculator.java
+++ src/main/java/namor/template/calculator/RPNcalculator.java
@@ -1,117 +1,70 @@
 package namor.template.calculator;
 
-import namor.template.stack.ArrayStack;
-import namor.template.stack.MapBasedStack;
 import namor.template.stack.Stack;
 
-public class RPNcalculator<T extends Number>{
-    private final Class<T> type;
-    private Stack<T> numbers;
+public class RPNcalculator {
 
-    public Stack<T> getNumbers() {
-        return numbers;
+    private Stack numbers;
+
+    public RPNcalculator(Stack stk) {
+        this.numbers = stk;
     }
 
-    public RPNcalculator(Class<T> type, Class stack) {
-        if (stack.equals(ArrayStack.class)){
-            numbers = new ArrayStack<>();
-        } else if (stack.equals(MapBasedStack.class)){
-            numbers = new MapBasedStack<>();
-        }
-        this.type = type;
-    }
-
-    private Class<T> getMyType() {
-        return this.type;
-    }
-
-    public T calculate(String arithmeticExpression) {
+    public double calculate(String arithmeticExpression) {
 
         for (String operationElement : arithmeticExpression.split(" ")) {
             if (Sign.isSign(operationElement)) {
                 doArithmeticOperation(operationElement, numbers);
             } else {
-                convertStringToNumber(getMyType(), numbers, operationElement);
+                numbers.push(Double.parseDouble(operationElement));
             }
         }
-        return numbers.pop();
+        return (double) numbers.pop();
     }
 
-    private void doArithmeticOperation(String arithmeticOperator, Stack<T> numbers) {
+    private void doArithmeticOperation(String arithmeticOperator, Stack numbers) {
         switch (Sign.getValue(arithmeticOperator)) {
             case ADDITION:
-                numbers.push(addition(numbers.pop(), numbers.pop()));
+                numbers.push(addition(numbers));
                 break;
             case SUBTRACTION:
-                numbers.push(subtraction(numbers.pop(), numbers.pop()));
+                numbers.push(subtraction(numbers));
                 break;
             case MULTIPLICATION:
-                numbers.push(multiplication(numbers.pop(), numbers.pop()));
+                numbers.push(multiplication(numbers));
                 break;
             case DIVISION:
-                numbers.push(division(numbers.pop(), numbers.pop()));
+                numbers.push(division(numbers));
                 break;
         }
     }
 
-    private void convertStringToNumber(Class<T> type, Stack<T> numbers, String operationElement) {
-        if(type == Integer.class) {
-            numbers.push((T)(Integer)(Integer.parseInt(operationElement)));
-        } else if(type == Double.class) {
-            numbers.push((T)(Double)(Double.parseDouble(operationElement)));
-        } else if(type == Float.class) {
-            numbers.push((T)(Float)(Float.parseFloat(operationElement)));
-        } else {
-            numbers.push((T)(Long)(Long.parseLong(operationElement)));
-        }
+    private final double addition(Stack numbers) {
+        final double secondNumber = (Double) numbers.pop();
+        final double firstNumber = (Double) numbers.pop();
+
+        return firstNumber + secondNumber;
     }
 
-    private final <T> T addition(T firstNumber, T secondNumber) {
-        if (firstNumber.getClass() == Integer.class) {
-            return (T)(Integer)((Integer) secondNumber + (Integer) firstNumber);
-        } else if (firstNumber.getClass() == Double.class) {
-            return (T) (Double) ((Double) secondNumber + (Double) firstNumber);
-        } else if (firstNumber.getClass() == Float.class) {
-            return (T) (Float) ((Float) secondNumber + (Float) firstNumber);
-        } else {
-            return (T) (Long) ((Long) secondNumber + (Long) firstNumber);
-        }
+    private final double subtraction(Stack numbers) {
+        final double secondNumber = (Double) numbers.pop();
+        final double firstNumber = (Double) numbers.pop();
+
+        return firstNumber - secondNumber;
     }
 
-    private final <T> T subtraction(T firstNumber, T secondNumber) {
-        if (firstNumber.getClass() == Integer.class) {
-            return (T)(Integer)((Integer) secondNumber - (Integer) firstNumber);
-        } else if (firstNumber.getClass() == Double.class) {
-            return (T) (Double) ((Double) secondNumber - (Double) firstNumber);
-        } else if (firstNumber.getClass() == Float.class) {
-            return (T) (Float) ((Float) secondNumber - (Float) firstNumber);
-        } else {
-            return (T) (Long) ((Long) secondNumber - (Long) firstNumber);
-        }
+    private final double multiplication(Stack numbers) {
+        final double secondNumber = (Double) numbers.pop();
+        final double firstNumber = (Double) numbers.pop();
+
+        return firstNumber * secondNumber;
     }
 
-    private final <T> T multiplication(T firstNumber, T secondNumber) {
-        if (firstNumber.getClass() == Integer.class) {
-            return (T)(Integer)((Integer) secondNumber * (Integer) firstNumber);
-        } else if (firstNumber.getClass() == Double.class) {
-            return (T) (Double) ((Double) secondNumber * (Double) firstNumber);
-        } else if (firstNumber.getClass() == Float.class) {
-            return (T) (Float) ((Float) secondNumber * (Float) firstNumber);
-        } else {
-            return (T) (Long) ((Long) secondNumber * (Long) firstNumber);
-        }
-    }
+    private final double division(Stack numbers) {
+        final double secondNumber = (Double) numbers.pop();
+        final double firstNumber = (Double) numbers.pop();
 
-    private final <T> T division(T firstNumber, T secondNumber) {
-        if (firstNumber.getClass() == Integer.class) {
-            return (T)(Integer)((Integer) secondNumber / (Integer) firstNumber);
-        } else if (firstNumber.getClass() == Double.class) {
-            return (T) (Double) ((Double) secondNumber / (Double) firstNumber);
-        } else if (firstNumber.getClass() == Float.class) {
-            return (T) (Float) ((Float) secondNumber / (Float) firstNumber);
-        } else {
-            return (T) (Long) ((Long) secondNumber / (Long) firstNumber);
-        }
+        return firstNumber / secondNumber;
     }
 
 
--- src/test/java/namor/template/calculator/RPNcalculatorTest.java
+++ src/test/java/namor/template/calculator/RPNcalculatorTest.java
@@ -5,22 +5,20 @@ import namor.template.stack.MapBasedStack;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.zip.DeflaterOutputStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RPNcalculatorTest {
 
-    private RPNcalculator<Integer> rpnCalculator;
-
-    @Before
-    public void setUp() {
-        rpnCalculator = new RPNcalculator<>(Integer.class, ArrayStack.class);
-    }
+    private RPNcalculator rpnCalculator;
 
     @Test
     public void addTwoNumbers() {
-        final int expected = 6;
-        final int actual;
-        final String arithmeticExpression = "2 4 +";
+        final double expected = 6.4;
+        final double actual;
+        rpnCalculator = new RPNcalculator(new ArrayStack<Double>());
+        final String arithmeticExpression = "2.1 4.3 +";
 
         actual = rpnCalculator.calculate(arithmeticExpression);
         assertThat(actual).isEqualTo(expected);
@@ -28,9 +26,10 @@ public class RPNcalculatorTest {
 
     @Test
     public void substactTwoNumbers() {
-        final int expected = 5;
-        final int actual;
-        final String arithmeticExpression = "10 5 -";
+        final double expected = 5;
+        final double actual;
+        rpnCalculator = new RPNcalculator(new ArrayStack<Float>());
+        final String arithmeticExpression = "10f 5f -";
 
         actual = rpnCalculator.calculate(arithmeticExpression);
         assertThat(actual).isEqualTo(expected);
@@ -38,8 +37,9 @@ public class RPNcalculatorTest {
 
     @Test
     public void multiplicateTwoNumbers() {
-        final int expected = 45;
-        final int actual;
+        final double expected = 45;
+        final double actual;
+        rpnCalculator = new RPNcalculator(new ArrayStack<Long>());
         final String arithmeticExpression = "9 5 *";
 
         actual = rpnCalculator.calculate(arithmeticExpression);
@@ -48,8 +48,9 @@ public class RPNcalculatorTest {
 
     @Test
     public void divideTwoNumbers() {
-        final int expected = 7;
-        final int actual;
+        final double expected = 7;
+        final double actual;
+        rpnCalculator = new RPNcalculator(new ArrayStack<Integer>());
         final String arithmeticExpression = "49 7 /";
 
         actual = rpnCalculator.calculate(arithmeticExpression);
@@ -58,50 +59,12 @@ public class RPNcalculatorTest {
 
     @Test
     public void whenTwoArithmaticOperatinsDoneReurnsCorrectNumber() {
-        final int expected = 11;
-        final int actual;
+        final double expected = 11;
+        final double actual;
+        rpnCalculator = new RPNcalculator(new ArrayStack<Integer>());
         final String arithmeticExpression = "49 7 / 4 +";
 
         actual = rpnCalculator.calculate(arithmeticExpression);
         assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void stackWorksWithFloat() {
-        RPNcalculator<Float> FrpnCalculator = new RPNcalculator<>(Float.class, ArrayStack.class);
-        final float expected = 4.4f;
-        final float actual;
-        final String arithmeticExpression = "2.2 3.3 + 1.1 -";
-
-        actual = FrpnCalculator.calculate(arithmeticExpression);
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void stackWorksWithDouble() {
-        RPNcalculator<Double> FrpnCalculator = new RPNcalculator<>(Double.class, ArrayStack.class);
-        final double expected = 4.4;
-        final double actual;
-        final String arithmeticExpression = "2.2 3.3 + 1.1 -";
-
-        actual = FrpnCalculator.calculate(arithmeticExpression);
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void stackWorksWithLong() {
-        RPNcalculator<Long> FrpnCalculator = new RPNcalculator<>(Long.class, ArrayStack.class);
-        final long expected = 4L;
-        final long actual;
-        final String arithmeticExpression = "2 3 + 1 -";
-
-        actual = FrpnCalculator.calculate(arithmeticExpression);
-        assertThat(actual).isEqualTo(expected);
-    }
-    
-    @Test
-    public void stackWorksWithMapBasedStack() {
-        RPNcalculator<Integer> mrpnCalculator = new RPNcalculator<>(Integer.class, MapBasedStack.class);
-        assertThat(mrpnCalculator.getNumbers().getClass()).isEqualTo(MapBasedStack.class);
     }
 }
