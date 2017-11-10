98f25807649882dcd084dcebb40a296cd58209da
NamorV <drednout309@gmail.com>
2017-11-10 17:02:09 +0100

    multiplicate method

--- src/main/java/namor/template/calculator/ClassicCalculator.java
+++ src/main/java/namor/template/calculator/ClassicCalculator.java
@@ -45,6 +45,10 @@ public class ClassicCalculator {
                 result = subtract(numbers);
                 numbers.push(result);
                 break;
+            case MULTIPLICATION:
+                result = multiplication(numbers);
+                numbers.push(result);
+                break;
         }
 
     }
@@ -61,5 +65,12 @@ public class ClassicCalculator {
         final double firstNumber = numbers.pop();
 
         return firstNumber - secondNumber;
+    }
+
+    private final double multiplication(Stack<Double> numbers) {
+        final double secondNumber = numbers.pop();
+        final double firstNumber = numbers.pop();
+
+        return firstNumber * secondNumber;
     }
 }
