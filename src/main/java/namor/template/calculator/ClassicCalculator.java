b0a400648349f18e61e2f096c6fb709d170fbff4
NamorV <drednout309@gmail.com>
2017-11-10 17:04:38 +0100

    divide method

--- src/main/java/namor/template/calculator/ClassicCalculator.java
+++ src/main/java/namor/template/calculator/ClassicCalculator.java
@@ -49,6 +49,10 @@ public class ClassicCalculator {
                 result = multiplication(numbers);
                 numbers.push(result);
                 break;
+            case DIVISION:
+                result = divide(numbers);
+                numbers.push(result);
+                break;
         }
 
     }
@@ -72,5 +76,12 @@ public class ClassicCalculator {
         final double firstNumber = numbers.pop();
 
         return firstNumber * secondNumber;
+    }
+
+    private final double divide(Stack<Double> numbers) {
+        final double secondNumber = numbers.pop();
+        final double firstNumber = numbers.pop();
+
+        return firstNumber / secondNumber;
     }
 }
