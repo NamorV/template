44b6412a2eaa3e7f34010500783a258f3359a5c6
NamorV <drednout309@gmail.com>
2018-02-28 17:37:16 +0100

    changes

--- src/main/java/namor/template/calculator/ClassicCalculator.java
+++ src/main/java/namor/template/calculator/ClassicCalculator.java
@@ -1,50 +1,70 @@
 package namor.template.calculator;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 
 public class ClassicCalculator {
 
-    private LinkedList<String> operands;
-    private List<String> operands1;
-
-    public ClassicCalculator() {
-        this.operands = new LinkedList<>();
-    }
 
     public double calculate(String arithmeticExpression) {
+        LinkedList<String> operands = new LinkedList<>();
+        String[] operationElements = arithmeticExpression.split(" ");
 
-        for (String operationElement : arithmeticExpression.split(" ")) {
-            operands.addLast(operationElement);
+        operands.addAll(Arrays.asList(operationElements));
+
+        while(operands.size() > 1) {
+            String first = operands.poll();
+            String sign = operands.poll();
+            String second = operands.poll();
+            doArithmeticOperation(operands, first, sign, second);
         }
 
-        operands1 = Collections.unmodifiableList(operands);
-        double result = Double.parseDouble(operands1.get(0));
-
-        for (int iterator = 1; iterator < operands1.size() - 1; iterator = iterator + 2) {
-            result = doArithmeticOperation(result, operands1.get(iterator), operands1.get(iterator + 1));
-        }
-
-        return result;
+        return Double.parseDouble(operands.poll());
     }
 
-    private double doArithmeticOperation (double result, String sign, String number) {
-         switch (Sign.getValue(sign)) {
+    private void doArithmeticOperation (LinkedList<String> operands, String firstNumber, String sign, String secondNumber) {
+        switch (Sign.getValue(sign)) {
             case ADDITION:
-                result = result + Double.parseDouble(number);
+                operands.addFirst(add(firstNumber, secondNumber));
                 break;
             case SUBTRACTION:
-                result = result - Double.parseDouble(number);
+                operands.addFirst(subtract(firstNumber, secondNumber));
                 break;
             case MULTIPLICATION:
-                result = result * Double.parseDouble(number);
+                operands.addFirst(multiply(firstNumber, secondNumber));
                 break;
             case DIVISION:
-                result = result / Double.parseDouble(number);
+                operands.addFirst(divide(firstNumber, secondNumber));
                 break;
         }
 
-        return result;
+    }
+
+    private final String add(String firstString, String secondString) {
+        final double secondNumber = Double.parseDouble(secondString);
+        final double firstNumber = Double.parseDouble(firstString);
+
+        return String.valueOf(firstNumber + secondNumber);
+    }
+
+    private final String subtract(String firstString, String secondString) {
+        final double secondNumber = Double.parseDouble(secondString);
+        final double firstNumber = Double.parseDouble(firstString);
+
+        return String.valueOf(firstNumber - secondNumber);
+    }
+
+    private final String multiply(String firstString, String secondString) {
+        final double secondNumber = Double.parseDouble(secondString);
+        final double firstNumber = Double.parseDouble(firstString);
+
+        return String.valueOf(firstNumber * secondNumber);
+    }
+
+    private final String divide(String firstString, String secondString) {
+        final double secondNumber = Double.parseDouble(secondString);
+        final double firstNumber = Double.parseDouble(firstString);
+
+        return String.valueOf(firstNumber / secondNumber);
     }
 }
